a88cde26a8f1dab0a0ca6eebfff2d1f91210645d
Lucas Garcia <lucas@codeccoop.org>
2025-10-29 15:29:09 +0100

    feat: allow invoice validation for thirdparties with tva intra

--- class/actions_autoverifactu.class.php
+++ class/actions_autoverifactu.class.php
@@ -375,7 +375,7 @@ class ActionsAutoverifactu extends CommonHookActions
                 $thirdparty = $object->thirdparty;
                 $valid_id = $thirdparty->id_prof_check(1, $thirdparty);
 
-                if (!$thirdparty->idprof1 || !$valid_id) {
+                if (!$valid_id && !$thirdparty->tva_intra) {
                     $label = $langs->trans('Veri*Factu requires invoice third parties to have a valid professional ID');
                     $button = dolGetButtonAction(
                         $label,
